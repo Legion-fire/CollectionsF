fa5836199e7646225d741c0680e5344c197287e4
Legion-fire <Legion-fire@yandex.ru>
2025-08-28 07:21:36 +0300

    Подсчёт элементов и тест

--- src/CollectionsF.java
+++ src/CollectionsF.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CollectionsF {
+public final class CollectionsF {
+    private CollectionsF() {}
+    public static <T> Map<T, Integer> countOccurrences(T[] array) {
+        if (array == null) throw new NullPointerException("Массив пуст!");
+        Map<T, Integer> counts = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            T e = array[i];
+            counts.merge(e, 1, Integer::sum);
+        }
+        return counts;
+    }
 }
